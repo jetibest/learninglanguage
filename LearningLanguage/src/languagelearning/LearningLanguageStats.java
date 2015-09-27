f450c02fb7c4f61b9e27b90b6d95e4b769e0e2f2
Roberts Mencis <roberts.mencis@gmail.com>
2015-09-27 20:49:02 +0200

    Output best policy

--- LearningLanguage/src/languagelearning/LearningLanguageStats.java
+++ LearningLanguage/src/languagelearning/LearningLanguageStats.java
@@ -5,10 +5,12 @@ import java.text.DecimalFormat;
 import languagelearning.actions.Action;
 import languagelearning.agents.Agent;
 import languagelearning.agents.AgentFactory;
-import languagelearning.agents.TDSarsaVacuumCleaner;
+import languagelearning.agents.GridObject;
+import languagelearning.agents.TDQLearningVacuumCleaner;
 import languagelearning.agents.TDVacuumCleaner;
 import languagelearning.env.Environment;
 import languagelearning.env.RunnableEnvironment;
+import languagelearning.policies.StateActionPolicy;
 import languagelearning.states.StateVariable;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
@@ -30,19 +32,22 @@ public class LearningLanguageStats {
 		
 		final int agentInitCount = 1;
 		
-		final double explorationRate = 0.05;
+		final double explorationRate = 0.1;
 		final double learningRate = 0.1;
 		final double futureRewardDiscountRate = 0.9;
 		
 		final Action[] possibleActions = new Action[]{Action.TURN_RIGHT,Action.MOVE_FORWARD,Action.COLLECT_DUST};
+		//final Action[] possibleActions = new Action[]{Action.MOVE_NORTH,Action.MOVE_EAST,Action.MOVE_SOUTH,Action.MOVE_WEST,Action.TURN_RIGHT,Action.MOVE_FORWARD,Action.COLLECT_DUST};
 		//final StateVariable[] possibleStateVariables = new StateVariable[]{StateVariable.DUST_BELOW,StateVariable.DUST_AHEAD,StateVariable.OBSTACLE_AHEAD};
-		final StateVariable[] possibleStateVariables = new StateVariable[]{StateVariable.DUST_BELOW,StateVariable.OBSTACLE_AHEAD};
+		final StateVariable[] possibleStateVariables = new StateVariable[]{StateVariable.DUST_BELOW,StateVariable.DUST_AHEAD,StateVariable.DUST_TWO_AHEAD,StateVariable.OBSTACLE_AHEAD};
+		//final StateVariable[] possibleStateVariables = new StateVariable[]{StateVariable.DUST_NORTH,StateVariable.DUST_EAST,StateVariable.DUST_SOUTH,StateVariable.DUST_WEST,StateVariable.OBSTACLE_NORTH,StateVariable.OBSTACLE_EAST,StateVariable.OBSTACLE_SOUTH,StateVariable.OBSTACLE_WEST};
+		//final StateVariable[] possibleStateVariables = new StateVariable[]{StateVariable.DUST_NORTH,StateVariable.DUST_EAST,StateVariable.DUST_SOUTH,StateVariable.DUST_WEST};
 		//final StateVariable[] possibleStateVariables = StateVariable.values();
 		
 		final AgentFactory agentFactory = new AgentFactory() {
 			@Override
 			public Agent produceAgent(int x, int y) {
-				TDVacuumCleaner agent = new TDSarsaVacuumCleaner(x,y);
+				TDVacuumCleaner agent = new TDQLearningVacuumCleaner(x,y);
 				agent.setExplorationRate(explorationRate);
 				agent.setLearningRate(learningRate);
 				agent.setFutureRewardDiscountRate(futureRewardDiscountRate);
@@ -57,6 +62,8 @@ public class LearningLanguageStats {
 		};
 		
 		DescriptiveStatistics collectedDustStats = new DescriptiveStatistics();
+		StateActionPolicy bestPolicy = null;
+		double maxCollectedDustRatio = 0;
 		
 		for (int run = 0; run < runs; run++) {
 			final Environment environment = new Environment(gridHeight,gridWidth) {
@@ -90,6 +97,21 @@ public class LearningLanguageStats {
 			double collectedDustRatio = (double)totalDustCollected / (double)totalDustBefore;
 			collectedDustStats.addValue(collectedDustRatio);
 			System.out.println("before = " + totalDustBefore + " after = " + totalDustAfter + " collected = " + totalDustCollected + " " + df.format(collectedDustRatio*100.0) + "%");
+		
+			if (collectedDustRatio > maxCollectedDustRatio) {
+				maxCollectedDustRatio = collectedDustRatio;
+				GridObject obj = environment.getObjects().get(0);
+				if (obj instanceof TDVacuumCleaner) {
+					TDVacuumCleaner td = (TDVacuumCleaner)obj;
+					bestPolicy = td.getPolicy();
+				}
+			}
+		}
+
+		if (bestPolicy != null) {
+			System.out.println();
+			System.out.println("Best policy with " + df.format(maxCollectedDustRatio*100.0) + "% dust collected:");
+			System.out.println(bestPolicy.toString());
 		}
 		
 		System.out.println();
