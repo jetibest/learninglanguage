b8e85b3e75570fdef3f369ec2da4ae929c227a07
Roberts Mencis <roberts.mencis@gmail.com>
2015-10-12 22:11:24 +0200

    paintComponent to separate methods

--- LearningLanguage/src/languagelearning/gui/LLPanel.java
+++ LearningLanguage/src/languagelearning/gui/LLPanel.java
@@ -59,103 +59,120 @@ public class LLPanel extends JPanel implements Runnable {
 		g.fillRect(0, 0, panelWidth, panelHeight);
 
 		if (LearningLanguage.MAIN.isRunning()) {
-			int[][] dustgrid = LearningLanguage.MAIN.getEnvironment()
-					.getDustGrid();
-			for (int i = 0; i < gridHeight; i++) {
-				int[] dustrow = dustgrid[i];
-				for (int j = 0; j < gridWidth; j++) {
-					int value = dustrow[j];
-					Color c = new Color(
-							(int) (255.0D * value / LearningLanguage.MAIN
-									.getEnvironment().getDustMax()), 0, 0);
-					g.setColor(c);
-					g.fillRect(j * gridSize, i * gridSize, gridSize, gridSize);
-				}
-			}
-
-			int[][] soundgridNew = LearningLanguage.MAIN.getEnvironment()
-					.getNewSoundGrid();
-			int[][] soundgridCurrent = LearningLanguage.MAIN.getEnvironment()
-					.getCurrentSoundGrid();
-			for (int i = 0; i < gridHeight; i++) {
-				int[] soundrowNew = soundgridNew[i];
-				int[] soundrowCurrent = soundgridCurrent[i];
-				for (int j = 0; j < gridWidth; j++) {
-					int valueNew = soundrowNew[j];
-					if (valueNew > 0) {
-						Color c = new Color(
-								(int) (255.0D * valueNew / Environment.SOUND_MAX),
-								(int) (255.0D * valueNew / Environment.SOUND_MAX),
-								(int) (255.0D * valueNew / Environment.SOUND_MAX));
-						g.setColor(c);
-						g.drawOval(j * gridSize, i * gridSize, gridSize, gridSize);
-					}
-					
-					int valueCurrent = soundrowCurrent[j];
-					if (valueCurrent > 0) {
-						Color c = new Color(
-								(int) (255.0D * valueCurrent / Environment.SOUND_MAX),
-								(int) (255.0D * valueCurrent / Environment.SOUND_MAX),
-								(int) (255.0D * valueCurrent / Environment.SOUND_MAX));
-						g.setColor(c);
-						g.drawOval((int)((j * gridSize)+(gridSize*0.25)), (int)((i * gridSize)+(gridSize*0.25)), (int)(gridSize/2), (int)(gridSize/2));
-					}
-				}
-			}
 			
-			int[][] pheromoneGrid = LearningLanguage.MAIN.getEnvironment().getPheromoneGrid();
-			for (int i = 0; i < gridHeight; i++) {
-				int[] pheromoneRow = pheromoneGrid[i];
-				for (int j = 0; j < gridWidth; j++) {
-					int pheromone = pheromoneRow[j];
-					for (int p = 0; p < pheromone; p++) {
-						Color c = Color.WHITE;
-						g.setColor(c);
-						int dx = (int)(Math.random() * gridSize);
-						int dy = (int)(Math.random() * gridSize);
-						g.drawOval((j * gridSize)+dx, (i * gridSize)+dy, 3, 3);
-						if (p > 50) {
-							break;
-						}
+			paintDustGrid(g);
+			
+			paintSoundGrid(g);
+			
+			paintPheromoneGrid(g);
+			
+			paintObjects(g);
+		}
+	}
+	
+	private void paintDustGrid(Graphics2D g) {
+		int[][] dustgrid = LearningLanguage.MAIN.getEnvironment()
+				.getDustGrid();
+		for (int i = 0; i < gridHeight; i++) {
+			int[] dustrow = dustgrid[i];
+			for (int j = 0; j < gridWidth; j++) {
+				int value = dustrow[j];
+				Color c = new Color(
+						(int) (255.0D * value / LearningLanguage.MAIN
+								.getEnvironment().getDustMax()), 0, 0);
+				g.setColor(c);
+				g.fillRect(j * gridSize, i * gridSize, gridSize, gridSize);
+			}
+		}
+	}
+	
+	private void paintSoundGrid(Graphics2D g) {
+		int[][] soundgridNew = LearningLanguage.MAIN.getEnvironment()
+				.getNewSoundGrid();
+		int[][] soundgridCurrent = LearningLanguage.MAIN.getEnvironment()
+				.getCurrentSoundGrid();
+		for (int i = 0; i < gridHeight; i++) {
+			int[] soundrowNew = soundgridNew[i];
+			int[] soundrowCurrent = soundgridCurrent[i];
+			for (int j = 0; j < gridWidth; j++) {
+				int valueNew = soundrowNew[j];
+				if (valueNew > 0) {
+					Color c = new Color(
+							(int) (255.0D * valueNew / Environment.SOUND_MAX),
+							(int) (255.0D * valueNew / Environment.SOUND_MAX),
+							(int) (255.0D * valueNew / Environment.SOUND_MAX));
+					g.setColor(c);
+					g.drawOval(j * gridSize, i * gridSize, gridSize, gridSize);
+				}
+				
+				int valueCurrent = soundrowCurrent[j];
+				if (valueCurrent > 0) {
+					Color c = new Color(
+							(int) (255.0D * valueCurrent / Environment.SOUND_MAX),
+							(int) (255.0D * valueCurrent / Environment.SOUND_MAX),
+							(int) (255.0D * valueCurrent / Environment.SOUND_MAX));
+					g.setColor(c);
+					g.drawOval((int)((j * gridSize)+(gridSize*0.25)), (int)((i * gridSize)+(gridSize*0.25)), (int)(gridSize/2), (int)(gridSize/2));
+				}
+			}
+		}
+	}
+	
+	private void paintPheromoneGrid(Graphics2D g) {
+		int[][] pheromoneGrid = LearningLanguage.MAIN.getEnvironment().getPheromoneGrid();
+		for (int i = 0; i < gridHeight; i++) {
+			int[] pheromoneRow = pheromoneGrid[i];
+			for (int j = 0; j < gridWidth; j++) {
+				int pheromone = pheromoneRow[j];
+				for (int p = 0; p < pheromone; p++) {
+					Color c = Color.WHITE;
+					g.setColor(c);
+					int dx = (int)(Math.random() * gridSize);
+					int dy = (int)(Math.random() * gridSize);
+					g.drawOval((j * gridSize)+dx, (i * gridSize)+dy, 3, 3);
+					if (p > 50) {
+						break;
 					}
 				}
-			}			
+			}
+		}			
+	}
+	
+	private void paintObjects(Graphics2D g) {
+		List<GridObject> gridObjects = LearningLanguage.MAIN
+				.getEnvironment().getGridObjects();
+		for (int i = 0; i < gridObjects.size(); i++) {
+			GridObject go = gridObjects.get(i);
 
-			List<GridObject> gridObjects = LearningLanguage.MAIN
-					.getEnvironment().getGridObjects();
-			for (int i = 0; i < gridObjects.size(); i++) {
-				GridObject go = gridObjects.get(i);
+			g.setColor(Color.GREEN);
+			g.fillOval(go.getX() * gridSize, go.getY() * gridSize,
+					gridSize, gridSize);
 
-				g.setColor(Color.GREEN);
-				g.fillOval(go.getX() * gridSize, go.getY() * gridSize,
-						gridSize, gridSize);
+			if (go instanceof Agent) {
+				Agent agent = (Agent) go;
 
-				if (go instanceof Agent) {
-					Agent agent = (Agent) go;
-
-					int halfGridSize = (int) (gridSize * 0.5);
-					int centerX = go.getX() * gridSize + halfGridSize;
-					int centerY = go.getY() * gridSize + halfGridSize;
-					int directionX = centerX;
-					int directionY = centerY;
-					switch (agent.getDirection()) {
-					case NORTH:
-						directionY = directionY - halfGridSize;
-						break;
-					case EAST:
-						directionX = directionX + halfGridSize;
-						break;
-					case SOUTH:
-						directionY = directionY + halfGridSize;
-						break;
-					case WEST:
-						directionX = directionX - halfGridSize;
-						break;
-					}
-
-					g.setColor(Color.BLUE);
-					g.drawLine(centerX, centerY, directionX, directionY);
+				int halfGridSize = (int) (gridSize * 0.5);
+				int centerX = go.getX() * gridSize + halfGridSize;
+				int centerY = go.getY() * gridSize + halfGridSize;
+				int directionX = centerX;
+				int directionY = centerY;
+				switch (agent.getDirection()) {
+				case NORTH:
+					directionY = directionY - halfGridSize;
+					break;
+				case EAST:
+					directionX = directionX + halfGridSize;
+					break;
+				case SOUTH:
+					directionY = directionY + halfGridSize;
+					break;
+				case WEST:
+					directionX = directionX - halfGridSize;
+					break;
 				}
+
+				g.setColor(Color.BLUE);
+				g.drawLine(centerX, centerY, directionX, directionY);
 			}
 		}
 	}
